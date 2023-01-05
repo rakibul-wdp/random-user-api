8996e085d29ba3e43d28727cf81d8aa2ba050376
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-05 19:53:03 +0600

    create route

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const routes = require('./routes/route');
 
-app.get('/', (req, res) => {
-  res.send('Hello World...!!!');
-})
+app.use('/user', routes);
 
 app.listen(port, () => {
   console.log(`Random User API listen on port ${port}`);
